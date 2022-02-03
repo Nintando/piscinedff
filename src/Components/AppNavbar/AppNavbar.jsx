d4152496bf66bc61c5ea62bc11788f5a5d5c03f9
Nintando <nintandot@gmail.com>
2022-02-03 11:18:36 +0100

    router

---
+++ src/Components/AppNavbar/AppNavbar.jsx
@@ -0,0 +1,26 @@
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export default function AppNavbar() {
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Navbar.Brand href="#home">App Carnet de Note</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link as={Link} to="/">
+							Accueil
+						</Nav.Link>
+						<Nav.Link as={Link} to="/statistiques">
+							Statistiques
+						</Nav.Link>
+						<Nav.Link as={Link} to="/carnet">
+							Carnets de notes
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	)
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AppNavbar from './Components/AppNavbar/AppNavbar'
+import AccueilPage from './Pages/AccueilPage/AccueilPage'
+import AddPage from './Pages/AddPage/AddPage'
+import UpdatePage from './Pages/UpdatePage/UpdatePage'
+import StatistiquesPage from './Pages/StatistiquesPage/StatistiquesPage'
+import CarnetPage from './Pages/CarnetPage/CarnetPage'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<Router>
+			<header>
+				<AppNavbar />
+			</header>
+			<Routes>
+				<Route path="/" element={<AccueilPage />} />
+				<Route path="/stats" element={<StatistiquesPage />} />
+				<Route path="/carnet" element={<CarnetPage />} />
+				<Route path="/carnet/add" element={<AddPage />} />
+				<Route path="/carnet/update:indice" element={<UpdatePage />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
