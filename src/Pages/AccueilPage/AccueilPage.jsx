4e904f8e55d8c690d46c4a1c8cd15de4f08a0ec1
Eclipse94w <ahfergani@gmail.com>
2022-02-03 17:20:09 +0100

    ajout du bootstrap et mise en page ++

--- src/Pages/AccueilPage/AccueilPage.jsx
+++ src/Pages/AccueilPage/AccueilPage.jsx
@@ -2,27 +2,27 @@ import { Container, Row, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default function HomePage() {
-	return (
-		<div className="App">
-			<main>
-				<Container>
-					<Row>
-						<div className="p-5 mb-4 bg-light rounded-3">
-							<Container fluid className="py-5">
-								<h1 className="display-5 fw-bold">BTC Single Page Application</h1>
-								<p className="col-md-8 fs-4">Piscine ReactJs</p>
-								&nbsp;
-								<Button className="m-2" variant="outline-primary" size="lg" as={Link} to="/stats">
-									Statistiques
-								</Button>
-								<Button variant="outline-primary" size="lg" as={Link} to="/carnet">
-									Carnet de notes
-								</Button>
-							</Container>
-						</div>
-					</Row>
-				</Container>
-			</main>
-		</div>
-	)
+return (
+<div className="App">
+	<main>
+		<Container>
+			<Row>
+				<div className="p-5 mb-4 bg-light rounded-3">
+					<Container fluid className="py-5">
+						<h1 className="display-5 fw-bold">BTC Single Page Application</h1>
+						<p className="col-md-8 fs-4">Piscine ReactJs</p>
+						&nbsp;
+						<Button className="m-2" variant="outline-primary" size="lg" as={Link} to="/stats">
+							Statistiques
+						</Button>
+						<Button variant="outline-primary" size="lg" as={Link} to="/carnet">
+							Carnet de notes
+						</Button>
+					</Container>
+				</div>
+			</Row>
+		</Container>
+	</main>
+</div>
+)
 }
--- src/Pages/AddPage/AddPage.jsx
+++ src/Pages/AddPage/AddPage.jsx
@@ -3,106 +3,92 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 
 export default function NotesAddPage() {
-	const [formAdd, setFormAdd] = useState({
-		titre: '',
-		categorie: '',
-		note: '',
-	})
+const [formAdd, setFormAdd] = useState({
+titre: '',
+categorie: '',
+note: '',
+})
 
-	const navigate = useNavigate()
+const navigate = useNavigate()
 
-	function add(e) {
-		e.preventDefault()
+function add(e) {
+e.preventDefault()
 
-		const id = Date.now()
-		let tmpForm = { ...formAdd }
-		tmpForm.id = id
-		setFormAdd(tmpForm)
+const id = Date.now()
+let tmpForm = { ...formAdd }
+tmpForm.id = id
+setFormAdd(tmpForm)
 
-		let notes = localStorage.getItem('notes')
-		if (notes === null) notes = '[]'
-		notes = JSON.parse(notes)
-		notes.push(tmpForm)
-		localStorage.setItem('notes', JSON.stringify(notes))
-		navigate('/carnet')
-	}
+let notes = localStorage.getItem('notes')
+if (notes === null) notes = '[]'
+notes = JSON.parse(notes)
+notes.push(tmpForm)
+localStorage.setItem('notes', JSON.stringify(notes))
+navigate('/carnet')
+}
 
-	return (
-		<>
-			<Container>
-				<Row>
-					<Col>
-						<h1>Ajouter une note</h1>
-						<hr />
-					</Col>
-				</Row>
+return (
+<>
+	<Container>
+		<Row>
+			<Col>
+			<h1>Ajouter une note</h1>
+			<hr />
+			</Col>
+		</Row>
 
-				<Row>
-					<Col md={6}>
-						<Form onSubmit={e => add(e)}>
-							<Form.Group className="mb-3">
-								<Form.Label>Titre</Form.Label>
-								<Form.Control
-									type="text"
-									placeholder="Entrer un titre"
-									value={formAdd.titre}
-									onChange={e => {
-										let tmp = { ...formAdd }
-										tmp.titre = e.target.value
-										setFormAdd(tmp)
-									}}
-									required
-								/>
-							</Form.Group>
-							<Form.Group className="mb-3">
-								<Form.Label className="m-2">Catégorie : </Form.Label>
-								<select
-									value={formAdd.categorie}
-									onChange={e => {
-										let tmp = { ...formAdd }
-										tmp.categorie = e.target.value
-										setFormAdd(tmp)
-									}}
-									required
-								>
-									<option>Choisir une catégorie</option>
-									<option value="Devoir">Devoir</option>
-									<option value="Jeux">Jeux</option>
-									<option value="Manga">Manga</option>
-								</select>
-							</Form.Group>
-							<Form.Group className="mb-3">
-								<Form.Label className="m-2">Note : </Form.Label>
-								<textarea
-									type="text"
-									placeholder="Entrer une note"
-									value={formAdd.note}
-									onChange={e => {
-										let tmp = { ...formAdd }
-										tmp.note = e.target.value
-										setFormAdd(tmp)
-									}}
-									required
-								/>
-							</Form.Group>
+		<Row>
+			<Col md={6}>
+			<Form onSubmit={e=> add(e)}>
+				<Form.Group className="mb-3">
+					<Form.Label>Titre</Form.Label>
+					<Form.Control type="text" placeholder="Entrer un titre" value={formAdd.titre} onChange={e=> {
+						let tmp = { ...formAdd }
+						tmp.titre = e.target.value
+						setFormAdd(tmp)}}
+						required/>
+				</Form.Group>
 
-							<hr />
+				<Form.Group className="mb-3">
+					<Form.Label className="m-2">Catégorie : </Form.Label>
+					<select class="form-select" value={formAdd.categorie} onChange={e=> {
+						let tmp = { ...formAdd }
+						tmp.categorie = e.target.value
+						setFormAdd(tmp)}}
+						required>
+						<option>Choisir une catégorie</option>
+						<option value="Devoir">Devoir</option>
+						<option value="Jeux">Jeux</option>
+						<option value="Manga">Manga</option>
+					</select>
+				</Form.Group>
 
-							<Button variant="light" as={Link} to="/carnet">
-								Retour
-							</Button>
+				<Form.Group className="mb-3">
+					<Form.Label className="m-2">Note : </Form.Label>
+					<textarea class="form-control" type="text" placeholder="Entrer une note" value={formAdd.note}
+						onChange={e=> {
+						let tmp = { ...formAdd }
+						tmp.note = e.target.value
+						setFormAdd(tmp)}}
+					required/>
+				</Form.Group>
+			<hr/>
+				<Button variant="light" as={Link} to="/carnet">
+				Retour
+				</Button>
 
-							<Button variant="outline-danger" className="float-end mx-2" type="reset">
-								Annuler
-							</Button>
+				<Button variant="outline-danger" className="float-end mx-2" type="reset">
+				Annuler
+				</Button>
 
-							<Button variant="success" type="submit" className="float-end">
-								Enregistrer
-							</Button>
-						</Form>
-					</Col>
-				</Row>
-			</Container>
-		</>
-	)
+				<Button variant="success" type="submit" className="float-end">
+				Enregistrer
+				</Button>
+
+			</Form>
+			</Col>
+		</Row>
+	</Container>
+</>
+)
 }
